03b5a468de1e33ad9502f4ca3b24f1736786e8c5
metalcupcake5 <imcoolcom5@gmail.com>
2022-10-20 01:21:43 -0700

    feat: replace unnecessary code

--- ws/server.js
+++ ws/server.js
@@ -2,15 +2,12 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-const connections = [];
-
 wss.on('connection', (ws) => {
-    connections.push(ws);
     console.log("new connection");
     ws.on('message', (data) => {
-        console.log(connections.length);
         console.log('received: %s', data);
-        for(const connection of connections) {
+        for(const connection of wss.clients) {
+            console.log(`ready state: ${connection.readyState}`);
             if(connection != ws){
                 connection.send(data.toString());
             }
