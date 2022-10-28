2fe2e2a59edb242c5d0a570ff4d71529ce0306bb
metalcupcake5 <imcoolcom5@gmail.com>
2022-10-28 02:13:39 -0700

    feat: assign ids to clients without a stored id

--- ws/client.js
+++ ws/client.js
@@ -5,7 +5,8 @@ const ws = new WebSocket('ws://localhost:8080');
 ws.on('open', () => {
     console.log("connection established")
     console.log("sent message")
-    ws.send('message from client 1');
+
+    ws.send(JSON.stringify({type: "message", message: 'message from client 1'}));
 });
 
 ws.on('message', (data) => {
--- ws/server.js
+++ ws/server.js
@@ -2,16 +2,29 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+let connections = 0;
+
 wss.on('connection', (ws) => {
     console.log("new connection");
     ws.on('message', (data) => {
         console.log('received: %s', data);
-        for(const connection of wss.clients) {
-            console.log(`ready state: ${connection.readyState}`);
-            if(connection != ws){
-                connection.send(data.toString());
+        data = JSON.parse(data.toString());
+        if(data?.id < 0) {
+            connections++;
+            const id = {
+                type: "data",
+                id: connections,
             }
-            
+            ws.send(JSON.stringify(id));
+        }
+        for(const connection of wss.clients) {
+            if(connection != ws){
+                const message = {
+                    type: "message",
+                    message: data.message,
+                }
+                connection.send(JSON.stringify(message));
+            }
         }
         //ws.send(`recieved the message "${data}"`)
     });
